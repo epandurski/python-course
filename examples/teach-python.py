a275d63431cde267cd758cc58a82fcc2e2bf72b9
Evgeni Pandurski <epandurski@gmail.com>
2018-04-28 13:20:07 +0300

    Add examples/

---
+++ examples/teach-python.py
@@ -0,0 +1,13 @@
+name = input('Enter a name: ')
+i = 0
+n = len(name)
+while i < n:
+    letter = name[i]
+    if letter == 'a':
+        print('The name contains "a".')
+        break
+    i = i + 1
+else:
+    print('The name does not contain "a".')
+
+print('Thank you for using this program.')
