37d3847e5f97a064adb9f9d0bb348543eadfe3c5
Evgeni Pandurski <epandurski@gmail.com>
2018-08-28 20:12:59 +0300

    Fix wrong example

--- examples/sqlite/sql_commands.py
+++ examples/sqlite/sql_commands.py
@@ -2,8 +2,8 @@ import sqlite3
 conn = sqlite3.connect('chinook.db')
 
 c = conn.cursor()
-db_result = c.execute("SELECT artistid FROM artists WHERE name = 'Accept'")
-accept_id = db_result.fetchone()[0]
+c.execute("SELECT artistid FROM artists WHERE name = 'Accept'")
+accept_id = c.fetchone()[0]
 print("Accept's ID is {}.".format(accept_id))
 print("Accept's albums:")
 albums = c.execute("SELECT albumid, title FROM albums WHERE artistid = ?", (2,))
